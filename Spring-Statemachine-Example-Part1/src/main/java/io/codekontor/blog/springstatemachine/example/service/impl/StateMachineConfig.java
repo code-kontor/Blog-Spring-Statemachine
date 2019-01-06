8f4c58b4276cce077576aa8adb7889facc59dcc7
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2019-01-06 11:47:38 +0100

    Added license information

--- Spring-Statemachine-Example-Part1/src/main/java/io/codekontor/blog/springstatemachine/example/service/impl/StateMachineConfig.java
+++ Spring-Statemachine-Example-Part1/src/main/java/io/codekontor/blog/springstatemachine/example/service/impl/StateMachineConfig.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (C) 2019 Code-Kontor GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Code-Kontor GmbH - initial API and implementation
+ ******************************************************************************/
 package io.codekontor.blog.springstatemachine.example.service.impl;
 
 import java.util.EnumSet;
